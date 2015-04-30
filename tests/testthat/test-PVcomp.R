958735bd721919b5b1af07d36906bb5c7f491cbf
jmonlong <jean.monlong@mail.mcgill.ca>
2015-04-30 13:46:56 -0400

    basic pv tests

--- tests/testthat/test-PVcomp.R
+++ tests/testthat/test-PVcomp.R
@@ -8,14 +8,36 @@ tre.dist = dist(t(tre.mat))
 geno.mat = matrix(sample(0:2, 30*3, replace=TRUE), 3)
 colnames(geno.mat) = samples
 geno.df = data.frame(snpId=paste0("snp",1:3), geno.mat)
+res.df = plyr::ldply(1:nrow(geno.df), function(ii){
+  compFscore(geno.df[ii,], tre.dist, tre.df)
+})
 
-test_that("Null pv are null",{})
+test_that("Results are reproducible",{
+  res1 = compPvalue(res.df, tre.dist)
+  res2 = compPvalue(res.df, tre.dist)
+  expect_true(all(abs(res1$pv-res2$pv)<.1))
+})
 
-test_that("Results are reproducible",{})
+test_that("Permutation and approximation give similar pvalues",{
+  res.perm = compPvalue(res.df, tre.dist, approx=FALSE)
+  res.approx = compPvalue(res.df, tre.dist, approx=TRUE)
+  expect_true(all(abs(res.perm$pv-res.approx$pv)<.1))
+})
 
-test_that("Permutation and approximation works",{})
+test_that("Pvalues for svQTLs work also",{
+  res.df = compPvalue(res.df, tre.dist, svQTL = TRUE, nb.perm.max=200)
+  expect_true(!is.null(res.df$pv.svQTL))
+})
 
-test_that("Pvalues for svQTLs work also",{})
-
-test_that("Minimum pvalue is never below expected",{})
+test_that("Minimum pvalue is never below expected",{
+  res.df$F[3] = res.df$F[3] * 1000
+  res.df = compPvalue(res.df, tre.dist, min.nb.ext.scores = 1, nb.perm.max = 10)
+  expect_true(all(res.df$pv>1/100))
+  res.df = compPvalue(res.df, tre.dist, min.nb.ext.scores = 100, nb.perm.max = 1000)
+  expect_true(all(res.df$pv>1/10000))
+  expect_true(all(res.df$pv[3]<1/100))
+  res.df = compPvalue(res.df, tre.dist, min.nb.ext.scores = 1000, nb.perm.max = 10000)
+  expect_true(all(res.df$pv>1/100000))
+  expect_true(all(res.df$pv[3]<1/1000))
+})
 
